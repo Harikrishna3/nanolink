d9a43454885ffb6ea7443dc9fcd5d2b6c864f934
harikrishna3 <harikrishnabbomen@gmail.com>
2026-02-21 13:13:00 +0530

    feat: enable global rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { RedisModule } from './redis/redis.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UrlModule, PrismaModule],
+  imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000, // 60 seconds (in milliseconds)
+      limit: 10,  // 10 requests allowed
+    }]),
+    UrlModule,
+    PrismaModule,
+    RedisModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD, // This makes the execution global
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
